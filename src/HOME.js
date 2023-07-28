a8a3b5f14c6ed9d6a7fcade63e7648f0e38a8ddb
Tuan Hiep <tansou57@gmail.com>
2023-07-29 01:59:40 +0700

    1:59

--- src/HOME.js
+++ src/HOME.js
@@ -1,23 +1,28 @@
-const path= require('path')
+const path = require('path')
 const express = require('express')
-const port = 3000
+const port = 4000
 const app = express()
 var cookieParser = require('cookie-parser')
 
 const handlebars = require('express-handlebars')
 //HTTP logger//
-const routes=require('./routes/router')
-const db=require('./app/models/db.model')
+const routes = require('./routes/router')
+const db = require('./app/models/db.model')
 db.connect()
 // app.use(morgan('combined'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
-
-app.use(express.static(path.join(__dirname,'public')));
-app.engine('.hbs',handlebars.engine({extname:'.hbs'}));
-app.set('view engine','hbs');
-app.set('views',path.join(__dirname,'resources','views'));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Thay đổi URL nguồn gốc tùy theo ứng dụng React của bạn
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 
 routes(app)
--- src/routes/student.js
+++ src/routes/student.js
@@ -4,20 +4,20 @@ const studentController=require('../app/controllers/StudentController');
 const loginController=require('../app/controllers/LoginController');
 
 
-router.get('/dangky',studentController.createView);
+router.get('api/dangky',studentController.createView);
 router.post('/store',studentController.createstudent);
 
-router.get('/login',loginController.checkLogined,loginController.login)
-router.post('/login',loginController.logincheck)
-router.get('/logout',loginController.logout)
+router.get('/api/login',loginController.checkLogined,loginController.login)
+router.post('/api/login',loginController.logincheck)
+router.get('api/logout',loginController.logout)
 
-router.get('/getallstudent',studentController.getAllStudents)
-router.post('/createstudent',studentController.createstudent)
+router.get('/api/getallstudents',studentController.getAllStudents)
+router.post('api/createstudent',studentController.createstudent)
 
 // router.get('/change/profile/MSSV=:id',studentController.findid);
 // router.post('/change/profile/MSSV=:id',studentController.change);
 
-router.get('/',loginController.authenticateToken,studentController.index);
+router.get('api/',loginController.authenticateToken,studentController.index);
 
 
 
