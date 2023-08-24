7f54c4d4c43a3930025d041d7cefda14b8792af1
Hiepprot3 <tansou57@gmail.com>
2023-08-25 00:38:31 +0700

    12:38

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const path = require('path')
 const cors=require('cors')
 require('dotenv').config()
-
+const http=require("http")
 const express = require('express')
-const port = process.env.APP_port
+const port = process.env.APP_port;
+const {Server}=require('socket.io')
 const app = express()
+const server = http.createServer(app);
+
 const bodyParser = require('body-parser');
 // const Host_URL = 'http://localhost:3000'
 const Host_URL='https://tuanhiepprot3.netlify.app';
-app.use(cors({
-    origin: Host_URL,
-    credentials: true,
-    exposedHeaders: ["set-cookie"] ,
 
-  }));
 app.use(cors({
     origin: Host_URL,
     credentials: true,
@@ -44,8 +42,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
-
-
+const io=new Server(server,{
+  autoConnect: false,
+  cors:{
+    origin:Host_URL,
+    methods:["GET","POST"],
+  
+  },
+});
+io.on("connection",(socket)=>{
+  console.log(`User Connected: ${socket.id}`);
+  socket.on("SendMessage",(data)=> {
+    console.log(data)
+    socket.broadcast.emit("recevie_messsage",data)
+  })
+ 
+})
 routes(app)
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+server.listen(port, () => console.log(`App listening at http://localhost:${port}`))
