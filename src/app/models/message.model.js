1de94bfa203809425438f1dff40e697ab6e970c1
Hiepprot3 <tansou57@gmail.com>
2023-08-29 15:58:28 +0700

    oke

--- src/app/models/message.model.js
+++ src/app/models/message.model.js
@@ -1,14 +1,12 @@
 const { connect, dbconnection } = require('../models/db.model')
-const Message_Conversation =  ()=> {
+const Message_Conversation = () => {
 
 
 }
-Message_Conversation.insertConversation=async (data)=>
-{
+Message_Conversation.insertConversation = async (data) => {
     console.log("join")
-    const insert= await new Promise((resolve, reject) => {
-        dbconnection.query("INSERT INTO conversations SET ?",data,(err,result)=>
-        {
+    const insert = await new Promise((resolve, reject) => {
+        dbconnection.query("INSERT INTO conversations SET ?", data, (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -18,35 +16,50 @@ Message_Conversation.insertConversation=async (data)=>
     })
     return insert
 }
-Message_Conversation.findSender=async(user1,user2)=>{
-    const query="Select * from conversations where user1=? or user2=?"
-    const userSent=await new Promise((resolve, reject) => {
-        dbconnection.query(query,[user1,user2],(err,result)=>
-        {
-            if(err)
-            {
+Message_Conversation.findSender = async (user1, user2) => {
+    // const query="Select * from conversations where user1=? or user2=?"
+    const query = `SELECT c.user1,c.user2,m.content,m.conversation_id as id,
+    m.created_at
+FROM 
+    messages m
+JOIN (
+    SELECT 
+    	
+        conversation_id,
+        MAX(created_at) AS max_created_at
+    FROM 
+        messages
+    GROUP BY 
+        conversation_id
+    
+) AS latest_msg
+ON 
+    m.conversation_id = latest_msg.conversation_id
+    AND m.created_at = latest_msg.max_created_at INNER JOIN (SELECT c.user1,c.user2,c.id FROM conversations c WHERE c.user1= ? or c.user2=? ) AS c ON c.id = m.conversation_id GROUP BY m.conversation_id ORDER by m.created_at DESC;
+`
+
+    const userSent = await new Promise((resolve, reject) => {
+        dbconnection.query(query, [user1, user2], (err, result) => {
+            if (err) {
                 reject(err)
 
             }
-            else
-            {
+            else {
                 resolve(result)
             }
         })
     })
     return userSent
 }
-Message_Conversation.getNewestFromConversation=async(data)=>
-{
-    const query=`SELECT m.id,m.sender_id,m.content, m.conversation_id, m.created_at 
+Message_Conversation.getNewestFromConversation = async (data) => {
+    const query = `SELECT m.id,m.sender_id,m.content, m.conversation_id, m.created_at 
     FROM messages AS m 
     INNER JOIN 
     (SELECT conversation_id, MAX(created_at) AS max_created_at 
     FROM messages GROUP BY conversation_id) AS subquery ON m.conversation_id = subquery.conversation_id AND m.created_at = subquery.max_created_at WHERE m.conversation_id=?;`;
 
-    const result= await new Promise((resolve, reject) => {
-        dbconnection.query(query,data,(err,result)=>
-        {
+    const result = await new Promise((resolve, reject) => {
+        dbconnection.query(query, data, (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -56,12 +69,10 @@ Message_Conversation.getNewestFromConversation=async(data)=>
     })
     return result
 }
-Message_Conversation.insertMess=async(data)=>
-{
-    const query="Insert into messages set ?";
-    const insertMess= await new Promise((resolve, reject) => {
-        dbconnection.query(query,data,(err,result)=>
-        {
+Message_Conversation.insertMess = async (data) => {
+    const query = "Insert into messages set ?";
+    const insertMess = await new Promise((resolve, reject) => {
+        dbconnection.query(query, data, (err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -71,11 +82,10 @@ Message_Conversation.insertMess=async(data)=>
     })
     return insertMess
 }
-Message_Conversation.FindMessFollowConver=async (data)=>{
-    const query="Select * from messages where conversation_id=?";
-    const result= await new Promise((resolve, reject) => {
-        dbconnection.query(query,data,(err,result)=>
-        {
+Message_Conversation.FindMessFollowConver = async (data) => {
+    const query = "Select * from messages where conversation_id=?";
+    const result = await new Promise((resolve, reject) => {
+        dbconnection.query(query, data, (err, result) => {
             if (err) {
                 reject(err);
             } else {
--- src/index.js
+++ src/index.js
@@ -65,14 +65,15 @@ const getUser = (userId) => {
 io.on("connection", (socket) => {
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
- 
-
+    console.log(socket.id)
+    console.log("connec",users)
  
     io.emit("getUsers", users);
   });
   socket.on("sendMessage", ({ sender_id, receiverId, content }) => {
     const user = getUser(receiverId)
     if (user) {
+      console.log(socket.id,"send to",user.socketId)
       io.to(user?.socketId).emit("getMessage", {
       
         sender_id,
@@ -84,6 +85,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     removeUser(socket.id);
     io.emit("getUsers", users);
+    // console.log("dis",users)
   });
 });
 routes(app)
